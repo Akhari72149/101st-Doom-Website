0ad80260bc0601c49455c8a563ca050b7569f4cc
Akhari <andkon72149@outlook.com>
2026-03-15 03:19:45 +0000

    GC update B3

--- src/app/Galactic-Campaign/page.tsx
+++ src/app/Galactic-Campaign/page.tsx
@@ -59,8 +59,8 @@ export default function GalacticCampaignPage() {
 
   /* ================= ANIMATED MILITARY COUNTERS ================= */
 
-  const cloneTarget = 569;
-  const droidTarget = 17998;
+  const cloneTarget = 619;
+  const droidTarget = 20432;
 
   const [cloneDisplay, setCloneDisplay] = useState(0);
   const [droidDisplay, setDroidDisplay] = useState(0);

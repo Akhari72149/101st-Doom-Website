c94354798b86c261014b63abfe6177bdcdabd86c
Akhari <andkon72149@outlook.com>
2026-03-30 16:06:39 +0100

    addition of Green Bean Machine

--- src/data/discordAnnouncementChannels.ts
+++ src/data/discordAnnouncementChannels.ts
@@ -2,4 +2,5 @@ export const discordAnnouncementChannels = [
   { id: "484243619310469121", name: "unit-announcements" },
   { id: "813532465062477854", name: "company-hq" },
   { id: "813532595149078589", name: "company-meeting-room" },
+  { id: "446059972418207753", name: "the-green-bean-machine" },
 ] as const;
